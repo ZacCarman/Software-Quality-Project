d48e8bdfc7a026539d1c1154676b0f9fa8217d09
ScottGarland <scott.a.garland@gmail.com>
2021-03-08 18:56:31 -0500

    adding to main

--- Testing.java
+++ Testing.java
@@ -139,27 +139,26 @@ public class Testing {
     public void MergeSort() {
         // Testing Merge Sort Ascending
         Random ran = new Random();
-        MergeSort msObj = new MergeSort();
         System.out.println("Testing Merge Sort");
 
         int[] Test1 = new int[] {1,4,3,5,2};
         int[] Sorted1 = new int[] {1,2,3,4,5};
-        assertTimeout(ofSeconds(1), () -> msObj.sort(Test1, 0, 4));
-        Assert.assertArrayEquals(Sorted1, msObj.sort(Test1, 0, 4));
+        assertTimeout(ofSeconds(1), () -> main.msort(Test1, 0, 4));
+        Assert.assertArrayEquals(Sorted1, main.msort(Test1, 0, 4));
 
         int[] Test2= new int[100];
         for(int i = 0; i < Test2.length; i++)
         {
             Test2[i] = ran.nextInt();
         }
-        assertTimeout(ofSeconds(1), () -> msObj.sort(Test2, 0, 99));
+        assertTimeout(ofSeconds(1), () -> main.msort(Test2, 0, 99));
 
         int[] Test3= new int[1000];
         for(int i = 0; i < Test3.length; i++)
         {
             Test3[i] = ran.nextInt();
         }
-        assertTimeout(ofSeconds(1), () -> msObj.sort(Test3, 0, 999));
+        assertTimeout(ofSeconds(1), () -> main.msort(Test3, 0, 999));
     }
 
     /*******************************************************************************************
--- main.java
+++ main.java
@@ -153,6 +153,72 @@ public class main {
     * Merge Sort in ascending and descending order.
     ********************************************************************************************/
     // see MergeSort.java
+
+    public static int[] merge(int arr[], int left, int mid, int right) {
+
+        int sizeL=  mid - left + 1; // size of left sub-array
+        int sizeR = right - mid; // size of right sub-array
+
+        //temporary sub-arrays for left and right
+        int leftArr[] = new int[sizeL];
+        int rightArr[] = new int[sizeR];
+
+        for (int i = 0; i < sizeL; ++i)
+            leftArr[i] = arr[left + i];
+
+        for (int j = 0; j < sizeR; ++j)
+            rightArr[j] = arr[mid + 1 + j];
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+
+        // Merging the 2 sub-arrays
+        while (i < sizeL && j < sizeR) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            }
+            else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // see if there are remaining elements left in the sub-arrays to copy over
+        while (i < sizeL) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (i < sizeR) {
+            arr[k] = rightArr[i];
+            j++;
+            k++;
+        }
+
+        return arr;
+    }
+
+    public static int[] msort(int[] arr, int left, int right) {
+        // arr is the input array, left is left most index, and right is right most. mid is middle
+
+        if (right > left) {
+            // we want the middle point
+            int mid = left + (right - 1) / 2;
+
+            // recursively sort the first and second sub-arrays
+            msort(arr, left, mid);
+            msort(arr, mid + 1, right);
+
+            // merge the 2 sub-arrays
+            merge(arr, left, mid, right);
+        }
+        return arr;
+    }
+
     /*******************************************************************************************
     * END SCOTT
     ********************************************************************************************/
