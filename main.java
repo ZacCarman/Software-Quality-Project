c632c02b75a89e807f7a808b7270d5523d0a616d
AlexanderPapachristu <55894236+AlexanderPapachristu@users.noreply.github.com>
2021-03-08 10:39:29 -0500

    Removed print function

--- main.java
+++ main.java
@@ -200,14 +200,4 @@ public class main {
         }
     }
 
-    /*Alexander Papachristu
-     *print the inputted array
-     */
-    public void printArray(int arr[])
-    {
-        for(int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-
 }
