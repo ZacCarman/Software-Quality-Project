38395da7e6d91c08fcbd10bf3367560152ec8dff
AlexanderPapachristu <55894236+AlexanderPapachristu@users.noreply.github.com>
2021-03-08 10:41:07 -0500

    Added printArray function

--- main.java
+++ main.java
@@ -199,5 +199,15 @@ public class main {
             heapify(arr,arrLength, largest); //recursively call heapify function
         }
     }
+	
+	 /*Alexander Papachristu
+     *print the inputted array
+     */
+    public void printArray(int arr[])
+    {
+        for(int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
 
 }
