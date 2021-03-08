54a0234b142ed8c799f391c97a85a1836188df5d
ScottGarland <scott.a.garland@gmail.com>
2021-03-08 11:28:00 -0500

    Create MergeSort.java

---
+++ MergeSort.java
@@ -0,0 +1,15 @@
+// Scott
+
+// take in an array of integers to mergesort
+// recursion, divide and conquer
+public void mergeSort(int[] array, int left, int right) {
+
+}
+
+public void merge(int[] array, int left, int middle, int right) {
+
+}
+
+public void sort(int[] array, int left, int middle, int right) {
+
+}
